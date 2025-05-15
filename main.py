27193011987817cd07f26149a0e992db6fe3dbb5
fnandojrs <106993281+fnandojrs@users.noreply.github.com>
2025-05-15 10:56:46 -0300

    Create main.py
    0001

---
+++ main.py
@@ -0,0 +1,16 @@
+from nicegui import ui, app
+
+app.add_static_files('/static', 'static')  # serve a pasta /static
+ui.add_head_html('<link rel="stylesheet" href="/static/custom.css">')  # Adiciona o CSS no <head>
+
+
+
+# Importa páginas
+import paginas.login
+import paginas.dashboard
+import paginas.cadastro_usuario
+import paginas.cadastro_livro
+import paginas.devolucao
+import paginas.consulta
+
+ui.run()
